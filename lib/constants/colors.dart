531c2445f11b72aa72989c5626f65f14ab818e6a
somya Gupta <somyaguptasg1282002@gmail.com>
2023-08-29 15:48:29 +0530

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0XffDa4048);
+const Color tdBlue = Color(0Xff5f52ee);
+const Color tdBlack = Color(0Xff3a3a3a);
+const Color tdGrey = Color(0Xff717171);
+const Color tdBGColor = Color(0Xffeeeff5);
